3b1bc4f3e53bed1fcba1b0d14efc08a12ee493b4
MPicco <martinalejandropicco1996@gmail.com>
2019-10-12 11:30:41 -0300

    Test refactor: Extract email parameter to common variable

--- spec/models/job_application_spec.rb
+++ spec/models/job_application_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe JobApplication do
+  let(:email) { 'applicant@test.com' }
+  let(:offer) { JobOffer.new }
+
   describe 'model' do
     it { is_expected.to respond_to(:applicant_email) }
     it { is_expected.to respond_to(:applicant_curriculum) }
@@ -9,21 +12,19 @@ describe JobApplication do
 
   describe 'create_for' do
     it 'should set applicant_email' do
-      email = 'applicant@test.com'
-      ja = described_class.create_for(email, JobOffer.new)
+      ja = described_class.create_for(email, offer)
       expect(ja.applicant_email).to eq(email)
     end
 
     it 'should set job_offer' do
-      offer = JobOffer.new
-      ja = described_class.create_for('applicant@test.com', offer)
+      ja = described_class.create_for(email, offer)
       expect(ja.job_offer).to eq(offer)
     end
   end
 
   describe 'process' do
     it 'should deliver contact info notification' do
-      ja = described_class.create_for('applicant@test.com', JobOffer.new)
+      ja = described_class.create_for(email, offer)
       expect(JobVacancy::App).to receive(:deliver).with(:notification, :contact_info_email, ja)
       ja.process
     end

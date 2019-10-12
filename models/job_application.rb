58b0f61604333f7dfa4863d6eac4f42a6bca3f26
MPicco <martinalejandropicco1996@gmail.com>
2019-10-12 14:37:05 +0000

    Test: JobApplication created with curriculum

--- models/job_application.rb
+++ models/job_application.rb
@@ -1,15 +1,14 @@
 class JobApplication
   attr_accessor :applicant_email
+  attr_accessor :applicant_curriculum
   attr_accessor :job_offer
 
-  def self.create_for(email, offer)
+  def self.create_for(email, curriculum, offer)
     app = JobApplication.new
     app.applicant_email = email
+    app.applicant_curriculum = curriculum
     app.job_offer = offer
     app
-  end
-
-  def applicant_curriculum
   end
 
   def process
--- spec/models/job_application_spec.rb
+++ spec/models/job_application_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe JobApplication do
   let(:email) { 'applicant@test.com' }
+  let(:curriculum) { 'linkedin.com/applicant.profile' }
   let(:offer) { JobOffer.new }
 
   describe 'model' do
@@ -12,19 +13,24 @@ describe JobApplication do
 
   describe 'create_for' do
     it 'should set applicant_email' do
-      ja = described_class.create_for(email, offer)
+      ja = described_class.create_for(email, curriculum, offer)
       expect(ja.applicant_email).to eq(email)
     end
 
     it 'should set job_offer' do
-      ja = described_class.create_for(email, offer)
+      ja = described_class.create_for(email, curriculum, offer)
       expect(ja.job_offer).to eq(offer)
+    end
+
+    it 'should set applicant_curriculum' do
+      ja = described_class.create_for(email, curriculum, offer)
+      expect(ja.applicant_curriculum).to eq(curriculum)
     end
   end
 
   describe 'process' do
     it 'should deliver contact info notification' do
-      ja = described_class.create_for(email, offer)
+      ja = described_class.create_for(email, curriculum, offer)
       expect(JobVacancy::App).to receive(:deliver).with(:notification, :contact_info_email, ja)
       ja.process
     end

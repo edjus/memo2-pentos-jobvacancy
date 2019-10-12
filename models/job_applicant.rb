6e1b5436b7509d73621c4982bf487bf794c6da82
MPicco <martinalejandropicco1996@gmail.com>
2019-10-12 12:53:18 -0300

    Tests: implement create_for and curriculum messages for JobApplicant

--- models/job_applicant.rb
+++ models/job_applicant.rb
@@ -1,4 +1,17 @@
 class JobApplicant
   attr_accessor :email
-  attr_accessor :curriculum
+  attr_writer :curriculum
+
+  def self.create_for(email, curriculum)
+    applicant = JobApplicant.new
+    applicant.email = email
+    applicant.curriculum = curriculum
+    applicant
+  end
+
+  def curriculum
+    return 'not specified' if @curriculum.nil? || @curriculum.empty?
+
+    @curriculum
+  end
 end

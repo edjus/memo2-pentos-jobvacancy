473d8bbddfd49c004fc53d94454ca3e638978bf1
kevinspasiuk <kevin_spasiuk@hotmail.com>
2019-10-13 13:16:25 -0300

    Range validation complete

--- models/remuneration_range.rb
+++ models/remuneration_range.rb
@@ -2,13 +2,23 @@ class RemunerationRange
   include ActiveModel::Validations
   attr_accessor :min, :max
 
-  validates :min, presence: true, unless: :max
-  validates :max, presence: true, unless: :min
+  validate :validation_positive
+  validate :validate_present
+
+  def validation_positive
+    errors.add(:min, :max, message: 'should be positive') if min.to_i.negative? ||
+                                                             max.to_i.negative?
+  end
+
+  def validate_present
+    errors.add(:min, :max, message: 'at least one should be present') if min.to_i.zero? &&
+                                                                         min.to_i.zero?
+  end
 
   def self.create_for(min_price, max_price)
     range = RemunerationRange.new
-    range.min = min_price
-    range.max = max_price
+    range.min = min_price.to_i
+    range.max = max_price.to_i
     range
   end
 end
--- spec/models/remuneration_range_spec.rb
+++ spec/models/remuneration_range_spec.rb
@@ -10,26 +10,36 @@ describe RemunerationRange do
 
   describe 'create_for' do
     it 'should be valid to create a range 2000 and 3000' do
-      remuneration_rage = described_class.create_for(2000, 3000)
-      expect(remuneration_rage.min).to eq(2000)
-      expect(remuneration_rage.max).to eq(3000)
+      remuneration_range = described_class.create_for(2000, 3000)
+      expect(remuneration_range.min).to eq(2000)
+      expect(remuneration_range.max).to eq(3000)
     end
   end
 
   describe 'valid?' do
     it 'should not be valid create a range 2000 and 3000' do
-      remuneration_rage = described_class.create_for(2000, 3000)
-      expect(remuneration_rage).to be_valid
+      remuneration_range = described_class.create_for(2000, 3000)
+      expect(remuneration_range).to be_valid
     end
 
     it 'should not be valid create a range null and null' do
-      remuneration_rage = described_class.create_for(nil, nil)
-      expect(remuneration_rage).not_to be_valid
+      remuneration_range = described_class.create_for(nil, nil)
+      expect(remuneration_range).not_to be_valid
     end
 
     it 'should be valid create a range 2000 and null' do
-      remuneration_rage = described_class.create_for(2000, nil)
-      expect(remuneration_rage).to be_valid
+      remuneration_range = described_class.create_for(2000, nil)
+      expect(remuneration_range).to be_valid
+    end
+
+    it 'should not be valid create a range -3000 and 3000' do
+      remuneration_range = described_class.create_for(-3000, 3000)
+      expect(remuneration_range).not_to be_valid
+    end
+
+    it 'should not be valid create a range 3000 and -3000' do
+      remuneration_range = described_class.create_for(3000, -3000)
+      expect(remuneration_range).not_to be_valid
     end
   end
 end

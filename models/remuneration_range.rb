0039222c439001bbfee4a34e14da9ae74f02df13
kevinspasiuk <kevin_spasiuk@hotmail.com>
2019-10-13 10:52:10 -0300

    Create for method

--- models/remuneration_range.rb
+++ models/remuneration_range.rb
@@ -1,3 +1,10 @@
 class RemunerationRange
   attr_accessor :min, :max
+
+  def self.create_for(min_price, max_price)
+    range = RemunerationRange.new
+    range.min = min_price
+    range.max = max_price
+    range
+  end
 end
--- spec/models/remuneration_range_spec.rb
+++ spec/models/remuneration_range_spec.rb
@@ -7,4 +7,12 @@ describe RemunerationRange do
     it { is_expected.to respond_to(:min) }
     it { is_expected.to respond_to(:max) }
   end
+
+  describe 'create_for' do
+    it 'should be valid to create a range 2000 and 3000' do
+      remuneration_rage = described_class.create_for(2000, 3000)
+      expect(remuneration_rage.min).to eq(2000)
+      expect(remuneration_rage.max).to eq(3000)
+    end
+  end
 end

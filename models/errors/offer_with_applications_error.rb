fa3631a85ce2ae825c314ffcae7bf718a66ba46b
kevinspasiuk <kevin_spasiuk@hotmail.com>
2019-10-19 12:23:11 -0300

    Model raise error if you try to delete an offer with applications

---
+++ models/errors/offer_with_applications_error.rb
@@ -0,0 +1,2 @@
+class OfferWithApplicationsError < RuntimeError
+end
--- spec/app/repositories/job_offer_repository_spec.rb
+++ spec/app/repositories/job_offer_repository_spec.rb
@@ -42,4 +42,24 @@ describe JobOfferRepository do
       expect(not_updated_offer.is_active).to eq true
     end
   end
+
+  describe 'delete job offer' do
+    let(:applicant) do
+      JobApplicant.create_for('applicant@test.com',
+                              'linkedin.com/applicant.profile')
+    end
+    let(:rem) { RemunerationRange.create_for(2000, 3000) }
+    let(:job_offer) do
+      JobOffer.new(title: 'a title',
+                   is_active: true,
+                   user_id: owner.id)
+    end
+
+    it 'should not be possible to delete offer with applicants' do
+      repository.save(job_offer)
+      app = JobApplication.create_for(applicant, job_offer, rem)
+      JobApplicationRepository.new.save(app)
+      expect { repository.destroy(job_offer) }.to raise_error(OfferWithApplicationsError)
+    end
+  end
 end
